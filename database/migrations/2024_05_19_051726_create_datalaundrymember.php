f851fe2bc8c12a5d8e9d602ce77244fdaf03d6ae
Boy Gautama <boy.gautama@gmail.com>
2024-05-19 12:47:21 +0700

    first commit

---
+++ database/migrations/2024_05_19_051726_create_datalaundrymember.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('datalaundrymember', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('member_id')->constrained('members');
+            $table->date('tgl_transaksi');
+            $table->enum('status_laundry', ['menunggu', 'diproses', 'selesai']);
+            $table->enum('status_pembayaran', ['bayar', 'belum']);
+            $table->string('lokasi_kirim');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('datalaundrymember');
+    }
+};
